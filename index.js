21f0492880accfbe1b194a8cf11e6376b95cc250
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-04 20:08:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Server is running on port',port);
+const express = require ('express');
+const app = express(); 
+
+app.get('/', function(req, res) {
+  res.send('hello world')
+})
+
+app.listen(8000,()=>{
+    console.log('Server is running on port')
+})
