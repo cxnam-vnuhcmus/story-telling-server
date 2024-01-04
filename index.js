bb84f21e67d34af9af8f2f3b27aaaa43a93e52c4
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-04 19:56:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 const express = require ('express');
-const mongoose = require('mongoose');
-const router = express.Router() 
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
 
-const Auth = require('./routes/auth')
-const UserRoute = require('./routes/user')
-const Voice = require('./routes/voice');
-const Genre = require('./routes/genre');
-const Stories = require('./routes/stories'); 
-const Wishlist = require('./routes/wishlist'); 
-const Playlist = require('./routes/playlist')
 const app = express(); 
 
 app.listen(8000,()=>{
