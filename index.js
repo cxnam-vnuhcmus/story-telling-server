b85a300cbda9c8f963289a516bc23260cd6e8b52
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-04 19:54:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ const Wishlist = require('./routes/wishlist');
 const Playlist = require('./routes/playlist')
 const app = express(); 
 
-require('dotenv').config();
-
-app.use('/uploads', cors(), express.static('uploads'));
-
-mongoose.connect('mongodb+srv://btlam20:SY4VdOVnVeDhoeke@storiestelling.fhfvgrl.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('DB Connected'))
-
-app.use(cors())
-app.use(cookieParser()); 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-
-const port = process.env.PORT || 8001;
-
-app.use('/api', Auth); 
-app.use('/api', UserRoute);
-app.use('/api', Voice); 
-app.use('/api', Genre); 
-app.use('/api', Stories); 
-app.use('/api', Wishlist); 
-app.use('/api', Playlist)
-
-
-app.listen(port,()=>{
+app.listen(8000,()=>{
     console.log('Server is running on port',port)
 })
