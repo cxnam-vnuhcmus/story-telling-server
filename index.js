bfc1f164bd07b3b5fb7ece59698ab93c410b75e0
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-04 23:03:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 8001;
 
+app.use('/api', Auth); 
+app.use('/api', UserRoute);
+app.use('/api', Voice); 
+app.use('/api', Genre); 
+app.use('/api', Stories); 
+app.use('/api', Wishlist); 
+app.use('/api', Playlist);
+
 app.get('/', function(req, res) {
     res.send('hello world');
 });
