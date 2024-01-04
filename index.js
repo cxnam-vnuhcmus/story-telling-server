33084e5ed1eba873228d3c41cad9ecbba59182c5
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-04 20:15:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const router = express.Router() 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const Auth = require('./routes/auth')
+const UserRoute = require('./routes/user')
+const Voice = require('./routes/voice');
+const Genre = require('./routes/genre');
+const Stories = require('./routes/stories'); 
+const Wishlist = require('./routes/wishlist'); 
+const Playlist = require('./routes/playlist')
 const app = express(); 
 
-app.get('/', function(req, res) {
-  res.send('hello world')
-})
+require('dotenv').config();
 
-const PORT = process.env.PORT || 5001;
+app.use('/uploads', cors(), express.static('uploads'));
 
-app.listen(PORT,()=>{
-    console.log('Server is running on port')
+mongoose.connect(process.env.DATABASE).then(()=>console.log('DB Connected'))
+
+app.use(cors())
+app.use(cookieParser()); 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const port = process.env.PORT || 5001;
+
+app.use('/api', Auth); 
+app.use('/api', UserRoute);
+app.use('/api', Voice); 
+app.use('/api', Genre); 
+app.use('/api', Stories); 
+app.use('/api', Wishlist); 
+app.use('/api', Playlist)
+
+
+app.listen(port,()=>{
+    console.log('Server is running on port',port)
 })
