97cd96ed596820409a39e152e1951e6f69b045cf
cxnam-vnuhcmus <66468063+cxnam-vnuhcmus@users.noreply.github.com>
2024-01-05 13:52:24 +0700

    Update stories.js

--- routes/stories.js
+++ routes/stories.js
@@ -1,63 +1,63 @@
 const express = require('express');
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 const storyController = require('../controllers/stories');
-// const multer = require('multer');
+const multer = require('multer');
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads/images');
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, file.fieldname + '-' + uniqueSuffix);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/images');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
-// const audiosStorage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const storyId = req.params.storyId;
-//     const userId = req.body.userId || req.query.userId;
-//     const voiceId = req.body.voiceId || req.query.voiceId;
-//     const userVoiceDir = `uploads/audios/${storyId}/${userId}/${voiceId}`;
+const audiosStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const storyId = req.params.storyId;
+    const userId = req.body.userId || req.query.userId;
+    const voiceId = req.body.voiceId || req.query.voiceId;
+    const userVoiceDir = `uploads/audios/${storyId}/${userId}/${voiceId}`;
 
-//     if (!fs.existsSync(userVoiceDir)) {
-//       fs.mkdirSync(userVoiceDir, { recursive: true });
-//     }
+    if (!fs.existsSync(userVoiceDir)) {
+      fs.mkdirSync(userVoiceDir, { recursive: true });
+    }
 
-//     cb(null, userVoiceDir);
-//   },
-//   filename: function (req, file, cb) {
-//     const storyId = req.params.storyId;
-//     const userId = req.body.userId || req.query.userId;
-//     const voiceId = req.body.voiceId || req.query.voiceId;
-//     const originalname = file.originalname;
-//     const audioFileName = `${storyId}-${userId}-${voiceId}-${Date.now()}${path.extname(originalname)}`;
+    cb(null, userVoiceDir);
+  },
+  filename: function (req, file, cb) {
+    const storyId = req.params.storyId;
+    const userId = req.body.userId || req.query.userId;
+    const voiceId = req.body.voiceId || req.query.voiceId;
+    const originalname = file.originalname;
+    const audioFileName = `${storyId}-${userId}-${voiceId}-${Date.now()}${path.extname(originalname)}`;
 
-//     cb(null, audioFileName);
-//   },
-// });
+    cb(null, audioFileName);
+  },
+});
 
-// const defaultStorageAudio = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         const storyId = req.params.storyId ; 
-//         const defaultVoiceDir = `uploads/audios/default/${storyId}`;
+const defaultStorageAudio = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const storyId = req.params.storyId ; 
+        const defaultVoiceDir = `uploads/audios/default/${storyId}`;
     
-//         if (!fs.existsSync(defaultVoiceDir)) {
-//           fs.mkdirSync(defaultVoiceDir, { recursive: true });
-//         }
+        if (!fs.existsSync(defaultVoiceDir)) {
+          fs.mkdirSync(defaultVoiceDir, { recursive: true });
+        }
     
-//         cb(null, defaultVoiceDir);
-//       },
-//       filename: function (req, file, cb) {
+        cb(null, defaultVoiceDir);
+      },
+      filename: function (req, file, cb) {
         
-//         const storyId = req.params.storyId;
-//         const audioFileName = `${storyId}${path.extname(file.originalname)}`;
+        const storyId = req.params.storyId;
+        const audioFileName = `${storyId}${path.extname(file.originalname)}`;
     
-//         cb(null, audioFileName);
-//       },
-// });
+        cb(null, audioFileName);
+      },
+});
 
 // const defualtUpload = multer({ storage: defaultStorageAudio });
 // const audiosUpload = multer({ storage: audiosStorage });
